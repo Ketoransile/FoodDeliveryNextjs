b241cc451cb665397edf93091e25be8378481211
Ketoran Sileshi <abdisileshi123@gmail.com>
2025-02-24 18:52:46 +0300

    Refactor: Move address schema to a separate file and update imports in address page

--- src/app/(checkout)/address/page.tsx
+++ src/app/(checkout)/address/page.tsx
@@ -15,19 +15,20 @@ import {
 import { Input } from "@/components/ui/input";
 import { redirect } from "next/navigation";
 import { useFormStore } from "@/stores/useFormStore";
-export const addressSchema = z.object({
-  name: z.string().min(2, { message: "Name must be at least 2 characters." }),
-  phoneNumber: z
-    .string()
-    .regex(/^\+?[1-9]\d{1,14}$/, { message: "Invalid phone number format." }),
-  streetName: z.string().min(3, { message: "Street name is required." }),
-  city: z.string().min(2, { message: "City is required." }),
-  state: z.string().min(2, { message: "State is required." }),
-  zip: z.string().regex(/^\d{4,5}(-\d{4})?$/, {
-    message:
-      "Invalid Zip Code. Should be 4 or 5 digits, optionally with '-XXXX'.",
-  }),
-});
+import { addressSchema } from "@/app/schemas/addressSchema";
+// export const addressSchema = z.object({
+//   name: z.string().min(2, { message: "Name must be at least 2 characters." }),
+//   phoneNumber: z
+//     .string()
+//     .regex(/^\+?[1-9]\d{1,14}$/, { message: "Invalid phone number format." }),
+//   streetName: z.string().min(3, { message: "Street name is required." }),
+//   city: z.string().min(2, { message: "City is required." }),
+//   state: z.string().min(2, { message: "State is required." }),
+//   zip: z.string().regex(/^\d{4,5}(-\d{4})?$/, {
+//     message:
+//       "Invalid Zip Code. Should be 4 or 5 digits, optionally with '-XXXX'.",
+//   }),
+// });
 export default function AddressPage() {
   const { address, setAddress } = useFormStore();
   const form = useForm<z.infer<typeof addressSchema>>({
