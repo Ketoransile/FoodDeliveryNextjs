2c52cfc8782448ef8284dc462642b085b8609183
Ketoran Sileshi <abdisileshi123@gmail.com>
2025-02-26 12:41:39 +0300

    Refactor: Remove unused code and replace Link with button in StepNavigation component

--- src/components/StepNavigation.tsx
+++ src/components/StepNavigation.tsx
@@ -1,56 +1,3 @@
-// "use client";
-// import React, { useState } from "react";
-// import Link from "next/link";
-
-// interface Step {
-//   label: string;
-//   link: string;
-//   isActive: boolean;
-// }
-
-// interface NavigationProps {
-//   steps: Step[];
-// }
-
-// const StepNavigation: React.FC<NavigationProps> = ({ steps }) => {
-//   const [activeIndex, setActiveIndex] = useState(
-//     steps.findIndex((step) => step.isActive)
-//   );
-
-//   return (
-//     <div className="flex items-center justify-center w-full">
-//       {steps.map((step, index) => {
-//         const isCompleted = index <= activeIndex;
-
-//         return (
-//           <React.Fragment key={index}>
-//             <Link href={step.link}>
-//               <div
-//                 className={`flex items-center justify-center w-10 h-10 rounded-full font-bold transition-all cursor-pointer
-//                 ${
-//                   isCompleted
-//                     ? "bg-blue-500 text-white"
-//                     : "bg-gray-300 text-gray-700"
-//                 }`}
-//                 onClick={() => setActiveIndex(index)}
-//               >
-//                 {index + 1}
-//               </div>
-//             </Link>
-//             {index < steps.length - 1 && (
-//               <div
-//                 className={`h-1 flex-1 mx-2 transition-all
-//                 ${isCompleted ? "bg-blue-500" : "bg-gray-300"}`}
-//               ></div>
-//             )}
-//           </React.Fragment>
-//         );
-//       })}
-//     </div>
-//   );
-// };
-
-// export default StepNavigation;
 "use client";
 
 import React from "react";
@@ -77,7 +24,8 @@ const StepNavigation: React.FC<NavigationProps> = ({ steps }) => {
 
         return (
           <React.Fragment key={step.link}>
-            <Link href={step.link}>
+            {/* <Link href={step.link}> */}
+            <button>
               <div
                 className={`flex items-center justify-center w-10 h-10 rounded-full font-bold transition-all cursor-pointer
                 ${
@@ -88,7 +36,7 @@ const StepNavigation: React.FC<NavigationProps> = ({ steps }) => {
               >
                 {index + 1}
               </div>
-            </Link>
+            </button>
             {index < steps.length - 1 && (
               <div
                 className={`h-1 flex-1 mx-2 transition-all 
